8cccf18aea13dc764cce5339eef33bde45fc317e
Áxel Medeiros <axel.cris2011@hotmail.com>
2018-02-01 14:53:20 -0300

    Finalizado pendencia na documentacao

--- SistemaAposta/src/sistema/Facade.java
+++ SistemaAposta/src/sistema/Facade.java
@@ -6,7 +6,7 @@ import easyaccept.EasyAccept;
  * Classe responsável de ser a fachada por onde acesso os controladores desse
  * sistema.
  * 
- * @author Áxel Medeiros
+ * @author Áxel Medeiros.
  */
 public class Facade {
 	ControleSistema controle;
@@ -63,43 +63,103 @@ public class Facade {
 	 * Exibe um cenário espeficico
 	 * 
 	 * @param numCenario
-	 *            representan um
+	 *            representa indice de um cenário.
+	 *            
 	 * @return Retorna a representação em String desses cenário escolhido
 	 */
 	public String exibirCenario(int numCenario) {
 		return this.controle.exibirCenario(numCenario);
 	}
 	
+	/**
+	 * Retorna uma representação de todos os cenários.
+	 * @return
+	 * 			Uma String com essa represeção.
+	 */
 	public String exibirCenarios() {
 		return this.controle.exibirTodosCenarios();
 	}
-
+	
+	/**
+	 * Cadastra uma aposta espeficia em um cenário.
+	 * @param cenario
+	 * 		representa o indice de um cenário
+	 * @param apostador
+	 * 		representa o nome do apostador.
+	 * @param valor
+	 * 		representa o valor a ser apostado
+	 * @param previsao
+	 * 		representa a previsão feita pelo usuário.
+	 */
 	public void cadastrarAposta(int cenario, String apostador, int valor,
 			String previsao) {
 		this.controle.adicionarApostaCenario(cenario, apostador, valor,
 				previsao);
 	}
-
+	
+	/**
+	 * Retorna o valor total das apostas feitas em um cenário.
+	 * @param numCenario
+	 * 		representa o indice de um cenário.
+	 * 	
+	 * @return
+	 * 		o valor total das apostas.
+	 */
 	public int valorTotalDeApostas(int numCenario) {
 		return this.controle.retornarValorTotalApostas(numCenario);
 	}
-
+	
+	/**
+	 * Retorna o número total de apostas em um cenário.
+	 * @param numCenario
+	 * 		representa o indice de um cenário.
+	 * @return
+	 */
 	public int totalDeApostas(int numCenario) {
 		return this.controle.retornarNumApostaCenario(numCenario);
 	}
-
+	
+	/**
+	 * Retorna uma representação de todas as apostas de um cenário.
+	 * @param numCenario
+	 * 		representa o indice de um cenário.
+	 * @return
+	 */
 	public String exibeApostas(int numCenario) {
 		return this.controle.exibirApostas(numCenario);
 	}
-
+	
+	/**
+	 * Fecha todas as apostas em um determinado cenário.
+	 * @param numCenario
+	 * 		representa o indice de um cenário.
+	 * @param resultadoCenario
+	 * 		bolean que representa se o cenário ocorreu ou não.
+	 */
 	public void fecharAposta(int numCenario, boolean resultadoCenario) {
 		this.controle.fecharAposta(numCenario, resultadoCenario);
 	}
-
+	
+	/**
+	 * Retorna a quantidade de caixa gerado por um cenário específico.
+	 * 
+	 * @param numCenario
+	 * 		representa o indice de um cenário.
+	 * 
+	 * @return
+	 * 		o valor total de caixa.
+	 */
 	public int getCaixaCenario(int numCenario) {
 		return this.controle.getCaixaCenario(numCenario);
 	}
-
+	
+	/**
+	 * Retorna o valor a ser distribuido pelo resultado de um cenário.
+	 * @param numCenario
+	 * 		representa o indice de um cenário.
+	 * @return
+	 * 		retorna o valor total de rateio de um cenário. 
+	 */
 	public int getTotalRateioCenario(int numCenario) {
 		return this.controle.getTotalRateioCenario(numCenario);
 	}
